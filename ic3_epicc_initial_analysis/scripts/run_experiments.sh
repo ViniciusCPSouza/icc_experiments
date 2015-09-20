7a4ca0a87934ebe9247fe4a9c57c126591dc2048
Vinicius Souza <vcps@cin.ufpe.br>
2015-09-19 22:02:43 -0300

    execution scripts

---
+++ ic3_epicc_initial_analysis/scripts/run_experiments.sh
@@ -0,0 +1,101 @@
+#/usr/bin/env bash
+
+# ---------------------------
+# Runs Epicc and IC3 on every apk from the 'apks' folder.
+#
+# Note: You have to run setup.sh first in order to create the symbolic links
+# needed by this script.
+#
+# Usage: ./run_experiments.sh
+# ---------------------------
+
+CONCURRENT_APKS=1
+
+APK_COUNT=0
+
+for apk in `ls apks`;
+do
+
+  APK_COUNT=$(($APK_COUNT + 1))
+
+  echo "----------------"
+  echo "Target APK #$APK_COUNT: $apk"
+
+  apk_path="`pwd`/apks/$apk"
+  apk_name="`echo $apk | cut -d . -f 1`"
+
+  echo -e "\n"
+  echo "Retargeting the application..."
+
+  # -- retargeting the application (.dex/.apk files --> .class files)
+  dare_output_dir="`pwd`/retargeted/$apk_name"
+  mkdir -p "$dare_output_dir"
+  ./dare -d "$dare_output_dir" "$apk_path" &> /dev/null
+
+  retargeted_dir="$dare_output_dir/retargeted/$apk_name"
+
+  # -- run epicc on the background and get its PID
+  epicc_cmd="java -Xmx2g -jar epicc -apk \"$apk_path\" -android-directory \
+            \"$retargeted_dir\" -cp android -icc-study epicc_results"
+
+  { time { $epicc_cmd > epicc_output; }; } &> eppic_time &
+
+  epicc_pid="$!"
+
+  # -- run ic3 on the background and get its PID
+  ic3_cmd="java -Xmx2g -jar ic3 -apkormanifest \"$apk_path\" \
+           -input \"$retargeted_dir\" -cp android -output ic3_results"
+
+  { time { $ic3_cmd > ic3_output; }; } &> ic3_time &
+
+  ic3_pid="$!"
+
+  # -- printing some info
+  echo -e "\n"
+  echo "Epicc is running on apk \"$apk\", process id: $epicc_pid"
+  echo "IC3 is running on apk \"$apk\", process id: $ic3_pid"
+
+  echo -e "\n"
+  echo "Waiting for Epicc and IC3 to finish their analysis..."
+
+  # waiting for the background jobs to finish
+  wait
+
+  echo -e "\n"
+  echo "Both Epicc and IC3 finished analysing the apk!"
+
+  # -- handling the results (files generated, time results, etc)
+
+  epicc_results_folder="results/$apk_name/epicc"
+  ic3_results_folder="results/$apk_name/icc"
+
+  mkdir -p $epicc_results_folder $ic3_results_folder
+
+  mv ic3_results/* $ic3_results_folder
+  mv epicc_results/* $epicc_results_folder
+
+  mv ic3_time $ic3_results_folder
+  mv epicc_time $epicc_results_folder
+
+  mv ic3_output $ic3_results_folder
+  mv epicc_output $epicc_results_folder
+
+  # -- remove all the data from this run
+  rm -rf ic3_results/*
+  rm -rf epicc_results/*
+
+  rm -f ic3_time
+  rm -f epicc_time
+
+  rm -f ic3_output
+  rm -f epicc_output
+
+done
+
+echo -e "\n"
+echo "****************"
+echo "All $APK_COUNT apks were analysed!"
+
+# TODO: Consolidate the results
+# -- Consolidating the results in a spreadsheet
+# ...
---
+++ ic3_epicc_initial_analysis/scripts/setup.sh
@@ -0,0 +1,27 @@
+#/usr/bin/env bash
+
+# ---------------------------
+# Prepares the symbolic links so the execution scripts can find the android, epicc and ic3 jars,
+# the dare program, and also the folder that contains the apks to be tested on.
+#
+# Android SDK: http://dl.google.com/android/android-sdk_r24.3.4-linux.tgz
+# IC3: https://github.com/siis/ic3/releases/download/v0.1.0/ic3-0.1.0-bin.tgz
+# Epicc dowload link: http://siis.cse.psu.edu/epicc/downloads/epicc-0.1.tgz
+# Dare: https://github.com/dare-android/platform_dalvik/releases/download/dare-1.1.0/dare-1.1.0-linux.tgz
+# Apks suggestion: https://github.com/secure-software-engineering/DroidBench
+#
+# Usage: ./setup.sh <path to the ic3 jar> <path to the epicc jar> <path to apks folder>
+# ---------------------------
+
+ANDROID_JAR="${1}"
+IC3_JAR="${2}"
+EPICC_JAR="${3}"
+DARE_PROGRAM="${4}"
+APKS_FOLDER="${5}"
+
+# create the links
+ln -s "${ANDROID_JAR}" android
+ln -s "${IC3_JAR}" ic3
+ln -s "${EPICC_JAR}" epicc
+ln -s "${DARE_PROGRAM}" dare
+ln -s "${APKS_FOLDER}" apks
